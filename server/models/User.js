d77c83fe8046b0c1a071158c30c17388ba188390
J.Cole <ccjohn015@gmail.com>
2022-05-02 19:24:15 -0500

    missing files

---
+++ server/models/User.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+const bcrypt = require('bcrypt');
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 7
+    }
+});

f1262e78507d2497877049f7c005d7c01a5c4277
Lindsey Dubnicka <lindseymiller2567@gmail.com>
2022-05-02 19:18:47 -0500

    created models and schemas

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,101 @@
+const { User, Post } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+// const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        // get logged in user's JWT token 
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id })
+                    .select('-passoword')
+                    .populate('posts')
+
+                return userData;
+            }
+
+            throw new AuthenticationError('Not logged in');
+        },
+        // get all users
+        users: async () => {
+            return User.find()
+                .select('-password')
+                .populate('posts')
+        },
+        // get a single user by username
+        user: async (parent, args) => {
+            return User.findOne(args.username)
+                .select('-password')
+                .populate('posts')
+        },
+        // get all posts with option to get all posts by username
+        posts: async (parent, { username }) => {
+            const params = username ? { username } : {};
+            return Post.find(params)
+                .sort({ createdAt: -1 })
+                .select('-__v')
+        },
+        // get a single post by _id
+        post: async (parent, { _id }) => {
+            return Post.findOne({ _id })
+                .select('-__v')
+        }
+    },
+
+    Mutation: {
+        // create new user
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+            return { token, user };
+        },
+        // log in user
+        login: async (parent, args) => {
+            const user = await User.findOne(args.email);
+            if (!user) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+            const correctPw = await user.isCorrectPassword(args.password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const token = signToken(user);
+
+            return { token, user };
+        },
+        // create new post 
+        addPost: async (parent, args, context) => {
+            if (context.user) {
+                const post = await Post.create({ ...args, username: context.user.username });
+
+                await User.findByIdAndUpdate(
+                    { _id: context.user._id },
+                    { $push: { posts: post._id } },
+                    { new: true }
+                );
+
+                return post;
+            }
+
+            throw new AuthenticationError('You need to be logged in.')
+        },
+        // create new comment
+        addComment: async (parent, args, context) => {
+            if (context.user) {
+                const updatedPost = await Post.findOneAndUpdate(
+                    { _id: args.postId },
+                    { $push: { comments: args.commentBody } },
+                    { new: true }
+                );
+
+                return updatedPost
+            }
+
+            throw new AuthenticationError('You need to be logged in.')
+        }
+    }
+};
+
+module.exports = resolvers;

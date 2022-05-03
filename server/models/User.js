07b9f73efebb3a14ea14b125bb0cd886628849bc
Chris-McLeod2 <91762149+Chris-McLeod2@users.noreply.github.com>
2022-05-02 20:42:46 -0500

    save

--- server/models/User.js
+++ server/models/User.js
@@ -2,49 +2,6 @@ const { Schema, model } = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const userSchema = new Schema(
-<<<<<<< HEAD
-  {
-    username: {
-      type: String,
-      required: true,
-      unique: true,
-      trim: true
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      match: [/.+@.+\..+/, 'Must match an email address!']
-    },
-    password: {
-      type: String,
-      required: true,
-      minlength: 5
-    },
-  },
-);
-
-// set up pre-save middleware to create password
-userSchema.pre('save', async function(next) {
-  if (this.isNew || this.isModified('password')) {
-    const saltRounds = 10;
-    this.password = await bcrypt.hash(this.password, saltRounds);
-  }
-
-  next();
-});
-
-// compare the incoming password with the hashed password
-userSchema.methods.isCorrectPassword = async function(password) {
-  return bcrypt.compare(password, this.password);
-};
-
-
-
-const User = model('User', userSchema);
-
-module.exports = User;
-=======
     {
         username: {
             type: String,
@@ -89,4 +46,3 @@ userSchema.methods.isCorrectPassword = async function (password) {
 const User = model('User', userSchema);
 
 module.exports = User;
->>>>>>> 7dd49d1c8d6cebe8d86d4678ef87db8e9c58d92a

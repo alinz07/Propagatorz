479f66b125a4066b0a44b25dac5282df6d021df9
Tony Linz <tonyllinz@gmail.com>
2022-05-03 17:29:52 -0500

    user(s) post(s) & me queries written. Returned a lot of data we can trim after we finish components

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <ApolloProvider client={client}>
-      <Router>
-        <div>
-          <StoreProvider>
-            <Nav />
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/login" component={Login} />
-              <Route exact path="/signup" component={Signup} />
-              <Route component={NoMatch} />
-            </Switch>
-          </StoreProvider>
-        </div>
-      </Router>
-    </ApolloProvider>
-  );
+    return (
+        <div>Hello World</div>
+        // <ApolloProvider client={client}>
+        //     <Router>
+        //         <div>
+        //             <StoreProvider>
+        //                 <Nav />
+        //                 <Switch>
+        //                     <Route exact path="/" component={Home} />
+        //                     <Route exact path="/login" component={Login} />
+        //                     <Route exact path="/signup" component={Signup} />
+        //                     <Route component={NoMatch} />
+        //                 </Switch>
+        //             </StoreProvider>
+        //         </div>
+        //     </Router>
+        // </ApolloProvider>
+    );
 }
 
 export default App;

8ab369ae59414de7b70f3fc2c4bd98629180890d
Lindsey Dubnicka <lindseymiller2567@gmail.com>
2022-05-03 21:36:42 -0500

    added more post seed data

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -10,7 +10,7 @@ db.once('open', async () => {
         {
             title: 'Leaves Falling Off Daily',
             plantType: 'Fiddle Leaf Fig',
-            description: 'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+            description: 'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
             picture: 'https://res.cloudinary.com/dk53zrwwe/image/upload/v1651630945/Plants/fiddleleaffig_b2otqh.jpg',
             username: 'Betty',
             comments: [
@@ -27,7 +27,7 @@ db.once('open', async () => {
         {
             title: 'Yellow Leaves',
             plantType: 'Spider Plant',
-            description: 'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+            description: 'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
             picture: 'https://res.cloudinary.com/dk53zrwwe/image/upload/v1651630946/Plants/spiderplant_eoiulz.jpg',
             username: 'Betty',
             comments: [
@@ -41,13 +41,57 @@ db.once('open', async () => {
         {
             title: 'Shriveled Leaves',
             plantType: 'Rattlesnake Plant',
-            description: 'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+            description: 'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
             picture: 'https://res.cloudinary.com/dk53zrwwe/image/upload/v1651630946/Plants/rattlesnackplant_mbrnbx.webp',
             username: 'Scott',
             comments: [
                 {
                     commentBody: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
                     username: "Sara"
+                }
+            ]
+
+        },
+        {
+            title: 'Cactus Never Flowers',
+            plantType: 'Christmas Cactus',
+            description: 'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
+            picture: 'https://res.cloudinary.com/dk53zrwwe/image/upload/v1651631458/Plants/xmascactus_fncqkm.webp',
+            username: 'Nancy',
+            comments: [
+                {
+                    commentBody: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
+                    username: "Sara"
+                },
+                {
+                    commentBody: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
+                    username: "Scott"
+                },
+                {
+                    commentBody: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
+                    username: "Nancy"
+                }
+            ]
+
+        },
+        {
+            title: 'Leaves Not Splitting',
+            plantType: 'Monstera',
+            description: 'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
+            picture: 'https://res.cloudinary.com/dk53zrwwe/image/upload/v1651631762/Plants/monstera_vfj1sw.webp',
+            username: 'Betty',
+            comments: [
+                {
+                    commentBody: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
+                    username: "Sara"
+                },
+                {
+                    commentBody: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
+                    username: "Scott"
+                },
+                {
+                    commentBody: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
+                    username: "Nancy"
                 }
             ]
 
@@ -63,7 +107,7 @@ db.once('open', async () => {
         username: 'Betty',
         email: 'betty@email.com',
         password: 'password',
-        posts: [posts[0]._id, posts[1]._id]
+        posts: [posts[0]._id, posts[1]._id, posts[4]._id]
     });
 
     await User.create({
@@ -71,6 +115,13 @@ db.once('open', async () => {
         email: 'scott@email.com',
         password: 'password',
         posts: [posts[2]._id]
+    });
+
+    await User.create({
+        username: 'Nancy',
+        email: 'nancy@email.com',
+        password: 'password',
+        posts: [posts[3]._id]
     });
 
     await User.create({

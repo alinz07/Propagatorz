c962f78aa25c755147fdacd5bbe54bb0351e94f6
Tony Linz <tonyllinz@gmail.com>
2022-05-03 20:22:07 -0500

    set up app.js, made some changes to index.js at root, we might want to rethink signup and login components to be pages like shop shop

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,64 @@
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+    ApolloClient,
+    InMemoryCache,
+    ApolloProvider,
+    createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { StoreProvider } from "./utils/GlobalState";
+
+import SinglePost from "./pages/SinglePost";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+
+import Footer from "./components/Footer";
+import Nav from "./components/Nav";
+
+const httpLink = createHttpLink({
+    uri: "/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+    const token = localStorage.getItem("id_token");
+    return {
+        headers: {
+            ...headers,
+            authorization: token ? `Bearer ${token}` : "",
+        },
+    };
+});
+
+const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+});
 
 function App() {
     return (
-        <div>Hello World</div>
-        // <ApolloProvider client={client}>
-        //     <Router>
-        //         <div>
-        //             <StoreProvider>
-        //                 <Nav />
-        //                 <Switch>
-        //                     <Route exact path="/" component={Home} />
-        //                     <Route exact path="/login" component={Login} />
-        //                     <Route exact path="/signup" component={Signup} />
-        //                     <Route component={NoMatch} />
-        //                 </Switch>
-        //             </StoreProvider>
-        //         </div>
-        //     </Router>
-        // </ApolloProvider>
+        <ApolloProvider client={client}>
+            <Router>
+                <div>
+                    <StoreProvider>
+                        <Nav />
+                        <Switch>
+                            <Route exact path="/" component={Home} />
+                            <Route exact path="/login" component={Login} />
+                            <Route exact path="/signup" component={Signup} />
+                            <Route
+                                exact
+                                path="/singlePost/:id"
+                                component={SinglePost}
+                            />
+                            <Route component={NoMatch} />
+                        </Switch>
+                        <Footer />
+                    </StoreProvider>
+                </div>
+            </Router>
+        </ApolloProvider>
     );
 }
 

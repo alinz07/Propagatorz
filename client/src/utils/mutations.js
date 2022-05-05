fc4778c415e842bd0d489f254715906bc74f7ee1
Chris-McLeod2 <91762149+Chris-McLeod2@users.noreply.github.com>
2022-05-05 15:48:29 -0500

    working front end to backend logic for signup form

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -11,24 +11,19 @@ export const LOGIN = gql`
   }
 `;
 
+
 export const ADD_USER = gql`
-  mutation addUser(
-      $username: String!
-      $email: String!
-      $password: String!
-    ) {
-        addUser(
-            username: $username
-            email: $email
-            password: $password
-        ) {
-            token
-            user {
-              _id
-            }
-        }
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
     }
+  }
 `;
+
 
 export const ADD_POST = gql`
   mutation addPost(

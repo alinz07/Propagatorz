853da6e5ed0081cc383e3238114b19ffab0a2f0b
Tony Linz <tonyllinz@gmail.com>
2022-05-04 21:20:41 -0500

    change folder structure and add code to home.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // import {
 //     ApolloClient,
 //     InMemoryCache,
@@ -11,8 +11,8 @@ import React from "react";
 
 // import SinglePost from "./pages/SinglePost";
 // import Home from "./pages/Home";
-// import Login from "./pages/Login";
-// import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 // import Footer from "./components/Footer";
 // import Nav from "./components/Nav";

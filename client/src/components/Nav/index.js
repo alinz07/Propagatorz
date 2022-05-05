ed06d988a057c4afb82fa17745a043bb3fbf9017
J.Cole <ccjohn015@gmail.com>
2022-05-04 21:17:35 -0500

    added in Nav, footer and partial mutations

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,67 +1,69 @@
 import React from 'react';
-import Auth from '../../utils/auth';
-import { Link } from 'react-router-dom';
+//import Auth from '../../utils/auth';
+//import { Link } from 'react-router-dom';
 import Avatar from '@mui/material/Avatar'
+
 
 function Nav() {
   
-    function showNavigation() {
-        if (Auth.loggedIn()) {
-          return (
-            <ul className="flex-row">
+    // function showNavigation() {
+    //     if (Auth.loggedIn()) {
+    //       return (
+    //         <ul className="flex-row">
 
-                <li className="mx-1">
-                  <Link to="/Home">
-                     Posts
-                  </Link>
-                </li>
-                <li className="mx-1">
-                    <Link to="/SinglePost">
-                        Create Post
-                    </Link>
-                </li>
-              <li className="mx-1">
-                {/* this is not using the Link component to logout or user and then refresh the application to the start */}
-                <a href="/" onClick={() => Auth.logout()}>
-                  Logout
-                </a>
+    //             <li className="mx-1">
+    //               {/* {<Link to="/Home">
+    //                  Posts
+    //               </Link>} */}
+    //             </li>
+    //             <li className="mx-1">
+    //                 {/* <Link to="/SinglePost">
+    //                     Create Post
+    //                 </Link> */}
+    //             </li>
+    //           <li className="mx-1">
+    //             {/* this is not using the Link component to logout or user and then refresh the application to the start */}
+    //             <a href="/" onClick={() => Auth.logout()}>
+    //               Logout
+    //             </a>
                 
-              </li>
-            </ul>
-          );
-        } else {
-          return (
-            <ul className="flex-row">
-              <li className="mx-1">
-                <Link to="/signup">
-                  Signup
-                </Link>
-              </li>
-              <li className="mx-1">
-                <Link to="/login">
-                  Login
-                </Link>
-              </li>
+    //           </li>
+    //         </ul>
+    //       );
+    //     } else {
+    //       return (
+    //         <ul className="flex-row">
+    //           <li className="mx-1">
+    //             {/* <Link to="/signup">
+    //               Signup
+    //             </Link> */}
+    //           </li>
+    //           <li className="mx-1">
+    //             {/* <Link to="/login">
+    //               Login
+    //             </Link> */}
+    //           </li>
               
-            </ul>
-          );
-        }
-    }
+    //         </ul>
+    //       );
+    //     }
+    // }
 
     return (
         <header className="flex-row px-1">
             <h1>
-                <Link to="/">
+
                     <Avatar 
                     alt="plant image" 
-                    src="../assets/images/sad-plant.jpg"
-                    sx={{ width: 20, height: 20}}
+                    src={require("../../assets/Images/sad-plant.jpg")}
+                    sx={{ width: 50, height: 50}}
+                    variant="square"
                     ></Avatar>
-                    Propagatorz
-                </Link>
+                   Propagatorz
+                
             </h1>
             <nav>
-                {showNavigation()}
+                {/* {showNavigation()} */}
             </nav>
         </header>
     );

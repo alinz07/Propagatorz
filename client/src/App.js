424ce59a70da8b2a62459a8c072d5c4a72c806cb
Tony Linz <tonyllinz@gmail.com>
2022-05-04 19:53:53 -0500

    chris branch working

--- client/src/App.js
+++ client/src/App.js
@@ -1,49 +1,50 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import {
-    ApolloClient,
-    InMemoryCache,
-    ApolloProvider,
-    createHttpLink,
-} from "@apollo/client";
-import { setContext } from "@apollo/client/link/context";
-import { StoreProvider } from "./utils/GlobalState";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import {
+//     ApolloClient,
+//     InMemoryCache,
+//     ApolloProvider,
+//     createHttpLink,
+// } from "@apollo/client";
+// import { setContext } from "@apollo/client/link/context";
+// import { StoreProvider } from "./utils/GlobalState";
 
-import SinglePost from "./pages/SinglePost";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
+// import SinglePost from "./pages/SinglePost";
+// import Home from "./pages/Home";
+// import Login from "./pages/Login";
+// import Signup from "./pages/Signup";
 
-import Footer from "./components/Footer";
-import Nav from "./components/Nav";
+// import Footer from "./components/Footer";
+// import Nav from "./components/Nav";
 
-const httpLink = createHttpLink({
-    uri: "/graphql",
-});
+// const httpLink = createHttpLink({
+//     uri: "/graphql",
+// });
 
-const authLink = setContext((_, { headers }) => {
-    const token = localStorage.getItem("id_token");
-    return {
-        headers: {
-            ...headers,
-            authorization: token ? `Bearer ${token}` : "",
-        },
-    };
-});
+// const authLink = setContext((_, { headers }) => {
+//     const token = localStorage.getItem("id_token");
+//     return {
+//         headers: {
+//             ...headers,
+//             authorization: token ? `Bearer ${token}` : "",
+//         },
+//     };
+// });
 
-const client = new ApolloClient({
-    link: authLink.concat(httpLink),
-    cache: new InMemoryCache(),
-});
+// const client = new ApolloClient({
+//     link: authLink.concat(httpLink),
+//     cache: new InMemoryCache(),
+// });
 
 function App() {
     return (
-        <ApolloProvider client={client}>
-            <Router>
-                <div>
-                    <StoreProvider>
-                        <Nav />
-                        <Switch>
+        // <ApolloProvider client={client}>
+        //     <Router>
+        <div>
+            {/* <StoreProvider> */}
+            <h1>hello</h1>
+            {/* <Nav /> */}
+            {/* <Switch>
                             <Route exact path="/" component={Home} />
                             <Route exact path="/login" component={Login} />
                             <Route exact path="/signup" component={Signup} />
@@ -53,12 +54,12 @@ function App() {
                                 component={SinglePost}
                             />
                             <Route component={NoMatch} />
-                        </Switch>
-                        <Footer />
-                    </StoreProvider>
-                </div>
-            </Router>
-        </ApolloProvider>
+                        </Switch> */}
+            {/* <Footer /> */}
+            {/* </StoreProvider> */}
+        </div>
+        //     </Router>
+        // </ApolloProvider>
     );
 }
 

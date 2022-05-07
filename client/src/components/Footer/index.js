4d0a70a788adfcb9065d6fde6214dc96ee6d9b0b
J.Cole <ccjohn015@gmail.com>
2022-05-07 15:04:39 -0500

    added in links for icons

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -5,10 +5,19 @@ import { green, red, yellow, deepPurple } from '@mui/material/colors'
 function Footer() {
     return(
         <Stack direction='row' spacing={5}>
-            <Avatar sx={{bgcolor: green[500]}}>T</Avatar>
-            <Avatar sx={{bgcolor: red[500]}}>Ch</Avatar>
-            <Avatar sx={{bgcolor: yellow[500]}}>L</Avatar>
-            <Avatar sx={{bgcolor: deepPurple[500]}}>C</Avatar>
+            <Avatar sx={{bgcolor: green[500]}} onClick={(e) => {
+                e.preventDefault();
+                window.location.href='https://github.com/alinz07'
+            }}>T</Avatar>
+            <Avatar sx={{bgcolor: red[500]}} onClick={(e) => {
+                e.preventDefault();
+                window.location.href='https://github.com/Chris-McLeod2'}}>Ch</Avatar>
+            <Avatar sx={{bgcolor: yellow[500]}} onClick={(e) => {
+                e.preventDefault();
+                window.location.href='https://github.com/lindseymiller2567'}}>L</Avatar>
+            <Avatar sx={{bgcolor: deepPurple[500]}} onClick={(e) => {
+                e.preventDefault();
+                window.location.href='https://github.com/ColeVibes'}}>C</Avatar>
         </Stack>
     )
 };

99803bf92f5a53af389f19685a8dc17e428b03e8
J.Cole <ccjohn015@gmail.com>
2022-05-07 23:58:49 -0500

    added in skele for single-post

--- client/src/pages/SinglePost.js
+++ client/src/pages/SinglePost.js
@@ -1,1 +1,11 @@
 import React from 'react';
+import { Card } from '@mui/material';
+
+
+const SinglePost = () => {
+    return (
+        <div className='container'>
+            <Card />
+        </div>
+    );
+};

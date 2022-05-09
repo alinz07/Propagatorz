6e6ef5e2991c1c55556cd45f4c898d245cd27d4b
J.Cole <ccjohn015@gmail.com>
2022-05-09 14:56:07 -0500

    started flesh single-page

--- client/src/pages/SinglePost.js
+++ client/src/pages/SinglePost.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import { Card } from '@mui/material';
+import { Link, useParams } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+import {
+    UPDATE_POST,
+    DELETE_POST,
+    DELETE_COMMENT,
+    ADD_COMMENT,
+    UPDATE_COMMENT
+} from '../utils/mutations';
+import { QUERY_ALL_POSTS } from '../utils/queries';
+
 
 
 const SinglePost = () => {
+
+    
     return (
         <div className='container'>
             <Card />

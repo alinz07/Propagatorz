a6e7fe878e6ef4bb96f2fe2e9e8b3f0e127bb6a5
J.Cole <ccjohn015@gmail.com>
2022-05-09 22:17:26 -0500

    added in single post and edited mutation

--- client/src/pages/SinglePost.js
+++ client/src/pages/SinglePost.js
@@ -1,51 +1,44 @@
-// import React, { useEffect, useState} from 'react';
-// import { CardList } from '../components/CardList';
-// // import {Link, useParams } from 'react-router-dom';
-// // import { useQuery, userQuery } from '@apollo/client';
-// // import { QUERY_ALL_POSTS } from '../utils/queries';
-// // import { useStoreContext } from '../utils/globalState';
-// // import { UPDATE_POSTS } from '../utils/actions';
-// import { StoreProvider, useStoreContext } from '../utils/globalState';
+import React, {useEffect, useState} from 'react';
+//import {Link, useParams} from 'react-router-dom';
+import {  useQuery } from '@apollo/client';
+import { UPDATE_POST } from '../utils/mutations';
+import {  QUERY_ONE_POST } from '../utils/queries';
+import {  useStoreContext } from '../utils/globalState';
+import CommentForm from '../components/CommentForm'
 
+const singlePost = () => {
+    const [state, dispatch] = useStoreContext();
 
+    const { loading, data } = useQuery(QUERY_ONE_POST);
 
-// const SinglePost = () => {
+    useEffect(() => {
+        if (data) {
+            dispatch({
+                type:UPDATE_POST,
+                post: data.post
+            });
+        }
+    }, [data,loading,disaptch]);
 
-//     // const [state, dispatch] = useStoreContext();
+    return (
+        <Grid container display="flex" wrap="wrap" justifyContent="center">
+            {filterPosts().map((post) => (
+                <PostCard
+                    key={post._id}
+                    id={post._id}
+                    title={post.title}
+                    commentCount={post.commentCount}
+                    comments={post.comments}
+                    createdAt={post.createdAt}
+                    description={post.description}
+                    picture={post.picture}
+                    plantType={post.plantType}
+                    username={post.username}
+                ></PostCard>
+            ))}
+            <CommentForm/>
+        </Grid>
+    );
+}
 
-//     // const { posts } = state;
-
-//     // const { id } = useParams();
-
-//     // const { currentPost, setCurrentPost } = useState({});
-
-//     // const { loading, data } = useQuery(QUERY_ALL_POSTS);
-
-//     // useEffect(() => {
-//     //     if (posts.length) {
-//     //         setCurrentPost(posts.find((post) => post._id === id));
-//     //     } else if(data) {
-//     //         dispatch({
-//     //             type: UPDATE_POSTS,
-//     //             posts: data.posts,
-//     //         });
-//     //         data.posts.forEach((post) => {
-
-//     //         })
-//     //     }
-//     // })
-
-//     const {}
-
-    
-//     return (
-//         <>
-//           {currentPost ? (
-//               <div className='container my-1'>
-//                   <Link to='/'>Return to Posts Page</Link>
-//               </div>
-//           ) : null}
-//           <Card/>
-//         </>
-//     );
-// };
+export default singlePost();

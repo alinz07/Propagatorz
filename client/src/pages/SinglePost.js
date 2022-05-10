09a75b30b3bdbbb3bf03634b623af4e768b2aeee
J.Cole <ccjohn015@gmail.com>
2022-05-09 19:48:56 -0500

    feature struggle

--- client/src/pages/SinglePost.js
+++ client/src/pages/SinglePost.js
@@ -1,38 +1,41 @@
 import React, { useEffect, useState} from 'react';
-import { Card } from '@mui/material';;
-import {Link, useParams } from 'react-router-dom';
-import { useQuery, userQuery } from '@apollo/client';
-import { QUERY_ALL_POSTS } from '../utils/queries';
-import { useStoreContext } from '../utils/globalState';
-import { UPDATE_POSTS } from '../utils/actions';
+import { CardList } from '../components/CardList';
+// import {Link, useParams } from 'react-router-dom';
+// import { useQuery, userQuery } from '@apollo/client';
+// import { QUERY_ALL_POSTS } from '../utils/queries';
+// import { useStoreContext } from '../utils/globalState';
+// import { UPDATE_POSTS } from '../utils/actions';
+import { StoreProvider, useStoreContext } from '../utils/globalState';
 
 
 
 const SinglePost = () => {
 
-    const [state, dispatch] = useStoreContext();
+    // const [state, dispatch] = useStoreContext();
 
-    const { posts } = state;
+    // const { posts } = state;
 
-    const { id } = useParams();
+    // const { id } = useParams();
 
-    const { currentPost, setCurrentPost } = useState({});
+    // const { currentPost, setCurrentPost } = useState({});
 
-    const { loading, data } = useQuery(QUERY_ALL_POSTS);
+    // const { loading, data } = useQuery(QUERY_ALL_POSTS);
 
-    useEffect(() => {
-        if (posts.length) {
-            setCurrentPost(posts.find((post) => post._id === id));
-        } else if(data) {
-            dispatch({
-                type: UPDATE_POSTS,
-                posts: data.posts,
-            });
-            data.posts.forEach((post) => {
+    // useEffect(() => {
+    //     if (posts.length) {
+    //         setCurrentPost(posts.find((post) => post._id === id));
+    //     } else if(data) {
+    //         dispatch({
+    //             type: UPDATE_POSTS,
+    //             posts: data.posts,
+    //         });
+    //         data.posts.forEach((post) => {
 
-            })
-        }
-    })
+    //         })
+    //     }
+    // })
+
+    const {}
 
     
     return (

f05f15ddc87e7075a054a90ca2d4c7da0e701e5c
Chris-McLeod2 <91762149+Chris-McLeod2@users.noreply.github.com>
2022-05-11 17:56:42 -0500

    heroku deploy

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,9 +1,8 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost:27017/propagatorz',
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  );
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/propagatorz", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+module.exports = mongoose.connection;
